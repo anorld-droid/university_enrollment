f3c99e12e79264f56126ff7723b63afe4f27ed33
Vincent Munene <munenevincent49@gmail.com>
2021-09-09 15:46:31 +0300

    added some json

--- Server.php
+++ Server.php
@@ -1,33 +1,36 @@
 <?php
+$fileInitialSize  = filesize("StudentData.txt");
+
 if (!empty($_POST['fName'])) {
-    $fName = $_POST["fName"] . "#";
-    $lName = $_POST["lName"] . "#";
-    $adm_number = $_POST["admNumber"] . "#";
-    $password = $_POST["pass"] . "#";
+
+    $myData = array("first name" => $_POST["fName"], "last name" => $_POST["lName"], "adm number" => $_POST["admNumber"], "password" => $_POST["pass"]);
+    //  $json = json_encode($myData);
+
+
 
     $my_file = "StudentData.txt";
 
-
+    $arr_data = array();
 
     if (file_exists($my_file)) {
-        $fp = fopen($my_file, "a") or die("Fail to open file");
-        $txt = $fName;
-        fwrite($fp, $txt);
-        $txt = $lName;
-        fwrite($fp, $lName);
-        $txt = $adm_number;
-        fwrite($fp, $txt);
-        $txt = $password;
-        fwrite($fp, $txt);
 
-        fclose($fp);
+        $file = fopen($my_file, 'a');
+        $jsondata = file_get_contents($my_file);
+        $arr_data = json_decode($jsondata, true);
+        $arr_data[] = $myData;
+
+
+        $jsondata = json_encode($arr_data);
+        file_put_contents($my_file, $jsondata);
+        $newFileSize = fstat($file);
+        if ($newFileSize['size'] > $fileInitialSize) {
+            echo "SUCCEFULLY REGISTERED" . '<br>';
+        }
+        fclose($file);
     } else {
         echo "file does not exist";
     }
-    echo "SUCCEFULLY REGISTERED" . '<br>';
-
-    $obj = array("first name" => $_POST["fName"], "last name" => $_POST["lName"], "adm number" => $_POST["admNumber"], "password" => $_POST["pass"]);
-    echo json_encode($obj);
+    // echo "SUCCEFULLY REGISTERED" . '<br>';
 } else {
 
     $admNumber = $_POST["adm_number"];
@@ -37,31 +40,31 @@ if (!empty($_POST['fName'])) {
 
     function search($input, $input2)
     {
+
         $check = 0;
         $my_file = "StudentData.txt";
         if (file_exists($my_file)) {
-            $handle = fopen($my_file, "r");
-            if ($handle) {
-                while (!feof($handle)) {
-                    $entry_array = explode("#", fgets($handle));
-                    for ($i = 0; $i < count($entry_array); $i++) {
-                        if ($entry_array[$i] === $input && $entry_array[$i + 1] === $input2) {
-                            echo "Welcome " . $entry_array[$i - 2] . " " . $entry_array[$i - 1] . "<br>";
 
-                            $obj =
-                                array("first name" => $entry_array[$i - 2], "last name" => $entry_array[$i - 1],  "adm Number" => $entry_array[$i], "password" => $entry_array[$i + 1]);
-                            echo json_encode($obj);
-                            $check += 1;
-                        }
-                    }
-                    if ($check === 0) {
-                        echo "User does not exist or has entered wrong details";
+            $obtainedData = json_decode(file_get_contents($my_file), true);
+
+
+            // $keys = array_keys($obtainedData);
+            foreach ($obtainedData as  $val) {
+                if ($val['adm number'] === $input) {
+                    $check++;
+                    if ($val['password'] === $input2) {
+                        echo "Logged In as " . $val['first name'] . " " . $val["last name"];
+                        break;
+                    } else {
+                        echo "Check password and try again";
+                        break;
                     }
                 }
-
-                fclose($handle);
             }
-            return NULL;
+
+            if ($check === 0) {
+                echo "User Has not Signed up";
+            }
         } else {
             echo "file does not exist";
         }
