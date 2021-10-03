1c1e44015e1e8538e7c268838066390c52a530ff
Vincent Munene <munenevincent49@gmail.com>
2021-10-03 10:18:45 +0300

    courses associtative array

---
+++ php/schools.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'config.php';
+require_once 'db.php';
+
+$db = connect(DB_SERVER, USER, PASSWORD, DB_NAME);
+
+
+$coursesSql = "SELECT courses.name,courses.school_id FROM courses INNER JOIN schools ON courses.school_id = schools.id";
+$schoolSql = "SELECT * FROM schools";
+
+$coursesResult = mysqli_query($db, $coursesSql);
+$schoolResult = mysqli_query($db, $schoolSql);
+$schooldata = [];
+$courseid = [];
+$coursename = [];
+$generalArray = [];
+$schoolid = [];
+
+
+while ($row = mysqli_fetch_assoc($schoolResult)) {
+    $schoolname[] = $row['name'];
+    $schoolid[]  = $row['id'];
+}
+while ($row = mysqli_fetch_assoc($coursesResult)) {
+    $courseid[] = $row['school_id'];
+    $coursename[] = $row['name'];
+}
+for ($i = 0; $i < mysqli_num_rows($schoolResult); $i++) {
+    $nameArray = [];
+    // $schoolIndex = $i + 1;
+
+    for ($j = 0; $j < mysqli_num_rows($coursesResult); $j++) {
+        if ($schoolid[$i] === $courseid[$j]) {
+            $nameArray[] = $coursename[$j];
+        }
+    }
+    $combinedArray = array(
+        $schoolname[$i] => array($nameArray[0], $nameArray[1], $nameArray[2], $nameArray[3])
+    );
+
+    $generalArray[] = $combinedArray;
+}
+$json_value = json_encode($generalArray);
+var_dump($json_value);
+
+$json_decoded = json_decode($json_value, true);
+var_dump($json_decoded);
--- search/dbOperation.php
+++ search/dbOperation.php
@@ -1,33 +1,23 @@
 <?php
 $searchQuery = $_REQUEST['query'];
 $formattedQuery = trim($searchQuery);
-$conn = mysqli_connect("localhost", "root", "", "student_query");
-
-$query = "CREATE DATABASE IF NOT EXISTS student_query";
-
-$sql1 = "CREATE TABLE IF NOT EXISTS query_storage (ID int(11) AUTO_INCREMENT,
-                      `name` varchar(255) NOT NULL,
-                     
-                      school varchar (255) NOT NULL,
-                      adm_number varchar(255) NOT NULL,
-                      handle varchar (255) NOT NULL,
-                      PRIMARY KEY  (ID))";
+$conn = mysqli_connect("localhost", "root", "", "student_details");
 
 
-
-$query2 = "SELECT * FROM query_storage WHERE `name`   LIKE  '" . $formattedQuery . "%'";
+$query2 = "SELECT * FROM student_data WHERE `fname`   LIKE  '" . $formattedQuery . "%'";
 $resultSet = mysqli_query($conn, $query2);
 
 if (mysqli_num_rows($resultSet)) {
     while ($row = mysqli_fetch_assoc($resultSet)) {
         echo "
         <tr>
-        <td>" . $row['id'] . "</td>
-         <td>" . $row['name'] . "</td>
-         
-           <td>" . $row['school'] . "</td>
-            <td>" . $row['adm_number'] . "</td>
-             <td>" . $row['handle'] . "</td>
+        <td>" . $row['ID'] . "</td>
+         <td>" . $row['fname'] . "</td>
+         <td>" . $row['lname'] . "</td>
+          <td>" . $row['adm_number'] . "</td>
+           <td>" . $row['status'] . "</td>
+           
+             <td>" . $row['completion'] . "</td>
 
         </tr>
         ";
