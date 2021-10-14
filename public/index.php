4fbbffcf784e1ea8ddec74de87a48be00162dcba
anorld-droid <mulindipatrice00@gmail.com>
2021-10-14 21:58:39 +0300

    removed redundant backstroke

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ $routes = array(
 );
 
 if (isset($routes[$request_uri[0]])) {
-    require '../app/' . $routes[$request_uri[0]] . '.php';
+    require '../app' . $routes[$request_uri[0]] . '.php';
 } else {
     //   header('HTTP/1.0 404 Not Found');
     //   require '../app/view/404.php';
